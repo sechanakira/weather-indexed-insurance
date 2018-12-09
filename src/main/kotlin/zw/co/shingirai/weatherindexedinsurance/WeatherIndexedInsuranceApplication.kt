a5468fab91b7595c677e7d75d3558d1926656407
Shingirai Chanakira <sechanakira@yahoo.com>
2018-12-09 19:04:50 +0200

    Initial Commit

---
+++ src/main/kotlin/zw/co/shingirai/weatherindexedinsurance/WeatherIndexedInsuranceApplication.kt
@@ -0,0 +1,11 @@
+package zw.co.shingirai.weatherindexedinsurance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeatherIndexedInsuranceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WeatherIndexedInsuranceApplication>(*args)
+}
